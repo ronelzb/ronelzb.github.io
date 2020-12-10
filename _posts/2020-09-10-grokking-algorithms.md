f03a0ad2775852b9675576f024b5ce7b69cf2192
ronelzb <ronelzb@gmail.com>
2020-12-09 18:40:37 -0600

    navbar improvement, post styling

--- _posts/2020-09-10-grokking-algorithms.md
+++ _posts/2020-09-10-grokking-algorithms.md
@@ -7,6 +7,8 @@ gh-badge: [watch, star, fork, follow]
 tags: [algorithms, arrays, breadth-first-search, dijkstra, dynamic-programming, k-nearest-neighbors, greedy, hashtable, python, quicksort, recursion, selection-sort]
 ---
 
+![Grokking Algorithms](/img/grokking-algorithms.jpg)
+
 *[Grokking Algorithms](https://www.manning.com/books/grokking-algorithms)* is a fully illustrated, friendly guide that teaches you how to apply common algorithms to the practical problems you face every day as a programmer.
 
 Check the project [here](https://github.com/ronelzb/grokking-algorithms/).
--- js/main.js
+++ js/main.js
@@ -16,14 +16,37 @@ $(function() {
      * Hide bootstrap navbar when scrolling
      */
     let $navbar = $('.navbar');
+    var navBarVisible = true;
     var prevScrollpos = $(window).scrollTop();
+    if (prevScrollpos > 0 && !$('.navbar').hasClass("custom-background")) {
+        $('.navbar').addClass("custom-background");
+    }
+
     $(window).scroll(function () {
         let currScrollpos = $(this).scrollTop();
         
-        if ($navbar.is(":visible") && currScrollpos > prevScrollpos) {
-            $navbar.slideUp();
-        } else if ($navbar.is(":hidden") && prevScrollpos > currScrollpos) {
-            $navbar.slideDown();
+        if (currScrollpos > prevScrollpos) {
+            //scrolling down
+            if (navBarVisible) {
+                navBarVisible = false;
+                $navbar.slideUp({
+                    complete: function() {
+                        if (!$('.navbar').hasClass("custom-background")) {
+                            $('.navbar').addClass("custom-background");
+                        }
+                    }
+                });
+            }
+            
+        } else { //(prevScrollpos > currScrollpos)
+            //scrolling up
+            if (!navBarVisible) {
+                navBarVisible = true;
+                $navbar.slideDown();
+            }
+            if (currScrollpos == 0 && $('.navbar').hasClass("custom-background")) {
+                $('.navbar').removeClass("custom-background");
+            }
         }
 
         prevScrollpos = currScrollpos;
