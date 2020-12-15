d4df2ce89665671792efbc33a523cf0df47989ff
ronelzb <ronelzb@gmail.com>
2020-12-14 22:42:08 -0600

    fixed navbar scrollUp and down minor issue

--- js/main.js
+++ js/main.js
@@ -3,37 +3,33 @@ $(function() {
      * Hide bootstrap navbar when scrolling
      */
     let $navbar = $('.navbar');
-    var navBarVisible = true;
-    var prevScrollpos = $(window).scrollTop();
-    if (prevScrollpos > 0 && !$('.navbar').hasClass("custom-background")) {
-        $('.navbar').addClass("custom-background");
+    var prevScrollpos = 0;
+
+    if($(window).scrollTop() < 100) {
+        $navbar.show();
     }
 
     $(window).scroll(function () {
         let currScrollpos = $(this).scrollTop();
-        
+
         if (currScrollpos > prevScrollpos) {
-            //scrolling down
-            if (navBarVisible) {
-                navBarVisible = false;
-                $navbar.slideUp({
-                    complete: function() {
-                        if (!$('.navbar').hasClass("custom-background")) {
-                            $('.navbar').addClass("custom-background");
-                        }
+            $navbar.slideUp({
+                complete: function() {
+                    if (!$(this).hasClass("custom-background")) {
+                        $(this).addClass("custom-background");
                     }
-                });
+                }
+            });
+        } else if (currScrollpos < prevScrollpos) {
+            if (currScrollpos < 100 && $navbar.hasClass("custom-background")) {
+                $navbar.removeClass("custom-background");
             }
             
-        } else { //(prevScrollpos > currScrollpos)
-            //scrolling up
-            if (!navBarVisible) {
-                navBarVisible = true;
-                $navbar.slideDown();
-            }
-            if (currScrollpos == 0 && $('.navbar').hasClass("custom-background")) {
-                $('.navbar').removeClass("custom-background");
-            }
+            $navbar.slideDown({
+                start: function() {
+                    $(this).css({ display: "flex" });
+                }
+            });
         }
 
         prevScrollpos = currScrollpos;
