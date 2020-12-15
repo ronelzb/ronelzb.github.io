7e3333f9be6e8c47c7e4c1ecd93e6be7a29330a9
ronelzb <ronelzb@gmail.com>
2020-12-14 19:46:34 -0600

    refactor tags page

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 $(function() {
     /**
-     * jquery.tagcloud.js
-     */
-    // only load tagcloud.js in tag.html
-    if($('#tag_cloud').length !== 0){
-        async('{{ "/js/jquery.tagcloud.js" | prepend: site.baseurl }}',function(){
-            $.fn.tagcloud.defaults = {
-                color: {start: '#bbbbee', end: '#0085a1'},
-            };
-            $('#tag_cloud a').tagcloud();
-        });
-    }
-
-    /**
      * Hide bootstrap navbar when scrolling
      */
     let $navbar = $('.navbar');
